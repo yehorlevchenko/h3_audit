e86dc79331a9dc36122c5e8bc91bcbd50f971c19
Nikita Khmelenko <cyberwitcher@gmail.com>
2020-09-15 12:26:58 +0300

    Improved CSS Selector References, minor fixes

--- extractor.py
+++ extractor.py
@@ -1,52 +1,51 @@
-from selectolax.parser import HTMLParser, Node
+from selectolax.parser import HTMLParser
 
 
 class Extractor(HTMLParser):
-
-    def __init__(self, html, detect_encoding=True,
+    """
+    Designed for extraction tags in self.tag_dict from HTML
+    """
+    def __init__(self, html_text, detect_encoding=True,
                  use_meta_tags=True, decode_errors=u'ignore'):
 
-        super().__init__(html, detect_encoding=detect_encoding,
+        super().__init__(html_text, detect_encoding=detect_encoding,
                          use_meta_tags=use_meta_tags,
                          decode_errors=decode_errors)
+        self.tag_dict = {'meta_title': 'meta[property="og:title"]',
+                         'meta_description': 'meta[property="og:description"]',
+                         'meta_keywords': 'meta[property="og:keywords"]',
+                         'h1': 'h1',
+                         'h2': 'h2',
+                         'h3': 'h3',
+                         'a': 'a[href^="https"], a[href^="http"]'
+                         }
 
-    def find_all(self, tag_list):
+    def find_all(self):
         """
-        :param tag_list: list(str)
-        :return:
+        Collects all node lists to dict
+        :return:dict(list(str))
         """
-        tag_dict = dict()
-        for tag in tag_list:
-            tag_dict[tag] = self.find_tag(tag)
-        return tag_dict
+        result_dict = dict()
+        for tag in self.tag_dict.keys():
+            result_dict[tag] = self.find_tag(self.tag_dict[tag])
+        return result_dict
 
     def find_tag(self, tag):
         """
+        Extracts all nodes with tag name from HTML
         :param tag:
         :return: list(str) - str is node code and text
         """
         node_list = list()
         all_nodes = self.css(tag)
         for node in all_nodes:
-            if self._validate_node(node):
+            if node:
                 node_list.append(node.html)
         return node_list
-
-    def _validate_node(self, node):
-        if node:
-            if node.tag == 'a':
-                try:
-                    link = node.attributes['href']
-                except AttributeError:
-                    return False
-                return not (link.startswith('mailto:') or link.startswith('file:'))
-        else:
-            return False
 
 
 if __name__ == "__main__":
     html = """
     """
     extractor = Extractor(html)
-    tag_list = ['title', 'description', 'keywords', 'h1', 'h2', 'h3', 'a']
-    print(extractor.find_all(tag_list))
+    print(extractor.find_all())
