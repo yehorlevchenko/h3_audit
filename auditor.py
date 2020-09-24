09f8fb6e3a1e4b9516c7b3aad7b7808e547c9623
Yehor Levchenko <yehorlevchenko@icloud.com>
2020-09-24 20:55:49 +0300

    rabbitmq interaction added

--- auditor.py
+++ auditor.py
@@ -1,3 +1,6 @@
+import rabbitpy
+import json
+
 from getter import Getter
 from extractor import Extractor
 from analyzer import Analyzer
@@ -9,6 +12,13 @@ class Auditor:
         self.extractor = Extractor()
         self.analyzer = Analyzer()
 
+    def run(self):
+        for message in rabbitpy.consume('amqp://localhost:5672', 'audit_start'):
+            in_data = json.loads(message.body.decode('utf8'))
+            result = self.work(in_data['url_list'])
+            message.ack()
+            print(result)
+
     def work(self, url_list):
         if isinstance(url_list, str):
             url_list = [url_list]
@@ -18,11 +28,14 @@ class Auditor:
             if raw_page_data['status_code'] == 200:
                 raw_tags = self.extractor.work(raw_page_data['html'])
                 results = self.analyzer.work(raw_tags)
-                print(results)
+                return results
+
+
 
 
 if __name__ == '__main__':
     a = Auditor()
-    a.work("http://python.org")
+    # a.work("http://python.org")
+    a.run()
 
 
