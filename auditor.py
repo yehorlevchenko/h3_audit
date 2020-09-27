7abfc2a3437c7a7d6875e252da521fcacb734739
Nikita Khmelenko <cyberwitcher@gmail.com>
2020-09-27 17:14:57 +0300

    Added threading to run() method.

--- auditor.py
+++ auditor.py
@@ -1,5 +1,7 @@
-import rabbitpy
 import json
+from queue import Queue
+import rabbitpy
+import threading
 
 from getter import Getter
 from extractor import Extractor
@@ -11,17 +13,30 @@ class Auditor:
         self.getter = Getter()
         self.extractor = Extractor()
         self.analyzer = Analyzer()
+        self.thread_amount = 3
 
     def run(self):
+        result_queue = Queue()
+        result_list = list()
         with rabbitpy.Connection('amqp://localhost:5672') as connection:
             with connection.channel() as channel:
                 queue = rabbitpy.Queue(channel, 'audit_start')
                 for message in queue.consume():
-                    # TODO: add try\except and publosh broken messages to
+                    # TODO: add try\except and publish broken messages to
                     #  audit_error queue
                     in_data = json.loads(message.body.decode('utf8'))
-                    result = self.work(in_data['url_list'])
-                    self.finish_task(channel, in_data, result)
+
+                    # Slicing data into list(list() * thread_amount)
+                    data_sliced = [in_data['url_list'][i::self.thread_amount] for i in range(self.thread_amount)]
+
+                    threads = [threading.Thread(target=lambda q, arg: q.put(self.work(arg)),
+                                                args=(result_queue, urls)) for urls in data_sliced]
+                    [thread.start() for thread in threads]
+                    [thread.join() for thread in threads]
+
+                    while not result_queue.empty():
+                        result_list.append(result_queue.get())
+                    self.finish_task(channel, in_data, result_list)
                     message.ack()
 
     def work(self, url_list):
