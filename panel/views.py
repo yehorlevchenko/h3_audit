a15c8a482f0a5f84627ab676ac574001d3ce6bea
Sentezy <sentezyq@gmail.com>
2020-10-12 13:38:51 +0300

    0018_registration_sentezy

--- panel/views.py
+++ panel/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 from .models import Audit
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 def index(request):
@@ -21,7 +21,7 @@ def my_audits(request):
     if request.user.is_authenticated:
         audits = Audit.objects.filter(owner_id=2)
         audits = "\n".join([f"{a.id} - {a.main_url}"
-                           for a in audits])
+                            for a in audits])
         return HttpResponse(f"Your audits: \n{audits}")
     else:
         return redirect('login_page')
@@ -44,3 +44,16 @@ def login_page(request):
     else:
         return render(request, template, context=context)
 
+
+def registration(request):
+    template = 'panel/registration.html'
+    if request.method == 'POST':
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, template, {'new_user': new_user})
+    else:
+        user_form = RegistrationForm()
+    return render(request, template, {'user_form': user_form})
