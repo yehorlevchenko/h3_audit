13e6c4922c0310ae7c3ab943c3eb6239e8ed9c28
Kateryna Kucherenko <kaisa.kucherenko@gmail.com>
2020-10-16 17:11:44 +0300

    Add to urls path for audit_results

--- panel/urls.py
+++ panel/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('my_audits', views.my_audits, name='my_audits'),
     path('login_page', views.login_page, name='login_page'),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('audit_results/<int:audit_id>', views.audit_results, name='audit_results')
 ]
