4ade70a120311ccf70044b7d32eaaf8b04119a69
Nikita Khmelenko <cyberwitcher@gmail.com>
2020-10-16 14:16:45 +0300

    Added 'Result' model and added to admin panel

--- panel/admin.py
+++ panel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Audit
+from .models import Audit, Result
 
 
 admin.site.register(Audit)
+admin.site.register(Result)
--- panel/models.py
+++ panel/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import validate_comma_separated_integer_list
 
 
 class Audit(models.Model):
@@ -8,3 +9,10 @@ class Audit(models.Model):
     owner_id = models.ForeignKey(settings.AUTH_USER_MODEL,
                                  on_delete=models.DO_NOTHING)
 
+
+class Result(models.Model):
+    audit_id = models.ForeignKey(Audit, on_delete=models.DO_NOTHING)
+    page_url = models.URLField(name="page_url", max_length=255,
+                               null=False, blank=False)
+    error_list = models.CharField(name="error_list", max_length=255, default=None,
+                                  validators=[validate_comma_separated_integer_list])
