3369a5e3d9b398e6c497ce403e7fbce70adce189
Kateryna Kucherenko <kaisa.kucherenko@gmail.com>
2020-10-16 17:13:24 +0300

    Add new models to admin

--- panel/admin.py
+++ panel/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Audit
+from .models import Audit, Check, AuditResults
 
 
 admin.site.register(Audit)
+admin.site.register(Check)
+admin.site.register(AuditResults)
