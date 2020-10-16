d519dae7cfa283a42a9e3654ea19e08d57585118
Kateryna Kucherenko <kaisa.kucherenko@gmail.com>
2020-10-16 17:10:56 +0300

    Add view for page audit_results

--- panel/views.py
+++ panel/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .models import Audit
+from .models import Audit, AuditResults, Check
 from .forms import LoginForm, RegisterForm
 
 
@@ -68,6 +68,31 @@ def register(request):
             form = RegisterForm()
             context['register_form'] = form
             return render(request, template, context)
+
+
+def audit_results(request, audit_id):
+    template = 'panel/audit_results.html'
+    checks = Check.objects.all()
+    checks_group_name = Check.get_group_name()
+    audit_obj = Audit.objects.get(pk=audit_id)
+    context = {'checks': checks, 'checks_group_name': checks_group_name, 'audit_obj': audit_obj}
+    if request.user.is_authenticated:
+        if request.user.id == audit_obj.owner_id_id:
+            urls_by_checks = dict()
+            audit_results = AuditResults.objects.filter(audit_id=audit_id)
+            for check in checks:
+                results = list()
+                for url_result in audit_results:
+                    if check.code_error == url_result.code_error:
+                        results.append(url_result)
+                urls_by_checks[check.code_error] = results
+            context.update({'audit_results': urls_by_checks})
+            return render(request, template, context)
+        else:
+            return redirect('my_audits')
+    else:
+        return redirect('login_page')
+
 
 
 
